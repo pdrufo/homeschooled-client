28e7eebe8cc8c3cd08ade7a15d983f5ed38a702b
Peter Rufo <peter@peterrufo.com>
2020-06-12 10:46:00 -0400

    formatting fix

--- src/Components/SchoolLogs/SchoolLogs.js
+++ src/Components/SchoolLogs/SchoolLogs.js
@@ -1,7 +1,6 @@
 import React from "react";
 import LogEntryTable from "../LogEntryTable/LogEntryTable";
 import "./SchoolLogs.css";
-import SearchBox from '../SearchBox/SearchBox'
 
 export default function SchoolLogs(props) {
   return (
@@ -9,17 +8,6 @@ export default function SchoolLogs(props) {
       <header role="banner">
         <h1>School Log</h1>
       </header>
-
-      <section>
-        <SearchBox />
-        {/* <form id="log-search">
-          <div className="form-section">
-            <label htmlFor="log-search">Search</label>
-            <input type="text" name="recipe-search" />
-            <input type="submit" value="Submit" />
-          </div>
-        </form> */}
-      </section>
 
       <section className="schoolLog-list">
         <LogEntryTable history={props.history} />
